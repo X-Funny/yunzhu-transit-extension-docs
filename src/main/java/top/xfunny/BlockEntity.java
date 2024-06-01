d75a8681eea185a598116f22818857684aac29a9
X-Funny <79325059+X-Funny@users.noreply.github.com>
2024-06-01 23:33:32 +0800

    电梯按钮测试

--- src/main/java/top/xfunny/BlockEntity.java
+++ src/main/java/top/xfunny/BlockEntity.java
@@ -3,6 +3,7 @@ package top.xfunny;
 
 import org.mtr.mapping.holder.Identifier;
 import org.mtr.mapping.registry.BlockEntityTypeRegistryObject;
+import org.mtr.mod.block.BlockLiftButtons;
 import top.xfunny.Blocks;
 import org.mtr.mod.block.BlockLiftPanelEven1;
 import top.xfunny.blocks.LiftScreen;
@@ -10,9 +11,13 @@ import top.xfunny.blocks.LiftScreen;
 public class BlockEntity {
     static {
         LIFTSCREEN = Init.REGISTRY.registerBlockEntityType(new Identifier(Init.MOD_ID, "lift_screen"), LiftScreen.BlockEntity::new, Blocks.LIFTSCREEN::get);
+        LIFT_BUTTONS_2 = Init.REGISTRY.registerBlockEntityType(new Identifier(Init.MOD_ID, "lift_buttons_1"), BlockLiftButtons.BlockEntity::new, Blocks.LIFT_BUTTONS_2::get);
+
 
     }
     public static final BlockEntityTypeRegistryObject<LiftScreen.BlockEntity> LIFTSCREEN;
+    public static final BlockEntityTypeRegistryObject<BlockLiftButtons.BlockEntity> LIFT_BUTTONS_2;
+
     public static void init(){
         Init.LOGGER.info("注册方块实体中");
     }
